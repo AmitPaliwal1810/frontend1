9c6f9f0d4a42cb704169050ad8a7ad41517bdb14
AmitPaliwal1810 <amitpaliwal259@gmail.com>
2021-05-31 10:09:52 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelector('.nav-links');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const toggleImg = document.querySelector('#toggle-img');
+
+hamburgerBtn.addEventListener('click',()=>{
+    // this is for toggling the the open class if already add then it will remove it otherwise opsite
+    navLinks.classList.toggle('open');
+    // here it will give you the output true or false 
+    //contains is a class which gives you true or false
+    isopen = navLinks.classList.contains('open');
+    if (isOpen) {
+        toggleImg.src = './img/icon-close.svg';
+     } else {
+        toggleImg.src = './img/icon-hamburger.svg';
+    }
+    
+})
