486d0feae528157225eaad2699b4d5a6e3613177
AmitPaliwal1810 <amitpaliwal259@gmail.com>
2021-06-01 18:29:14 -0600

    navbutton improve

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const navLinks = document.querySelector(".nav-links");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
-const toggleImg = document.querySelector("#toggle-img");
+const toggleImg = document.querySelector('#toggle-img');
 
 hamburgerBtn.addEventListener("click", () => {
-  // this is for toggling the the open class if already add then it will remove it otherwise opsite
-  navLinks.classList.toggle("open");
+  // this is for toggling the the open class if already add then it will remove it otherwise opposite
+  navLinks.classList.toggle('open');
   // here it will give you the output true or false
   //contains is a class which gives you true or false
-  isopen = navLinks.classList.contains("open");
-  if (isopen) {
-    toggleImg.src = "./img/icon-close.svg";
+  isOpen = navLinks.classList.contains('open');
+  if (isOpen) {
+    toggleImg.src = './images/icon-close.svg';
   } else {
-    toggleImg.src = "./img/icon-hamburger.svg";
+    toggleImg.src = './images/icon-hamburger.svg';
   }
 });
+
+// always use ' ' except " "
